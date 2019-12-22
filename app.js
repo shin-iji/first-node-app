ea273c6ce11691e72666a4ae9d2685043bacc1c1
Shindanai <siradanai27@gmail.com>
2019-12-22 15:42:08 +0700

    Export Variable from Object

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
-const Car = require('./car.js')
-const myCar = new Car(4, 'red', 'Pick-up')
-console.log("My car's color is " + myCar.color)
+const settings = require('./setting')
+const databaseName = settings.databaseName
+console.log(databaseName)
---
+++ setting.js
@@ -0,0 +1,6 @@
+const Settings = {
+    secretKey: 'GT3458_3!ky12Bb1KJjdr',
+    userPasss: 'ktD234BzKYjW22_!!rj',
+    databaseName: 'iosthailandDatabase'                    
+}
+module.exports = Settings
