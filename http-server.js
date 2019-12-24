39aa573aec7679d293473db90065401efc2573bd
Shindanai <siradanai27@gmail.com>
2019-12-24 14:49:09 +0700

    Http Module Http Request

--- http-server.js
+++ http-server.js
@@ -1,7 +1,15 @@
-const http = require('http')
-const server = http.createServer()
+const http = require('http');
+const server = http.createServer(function(req, res) {
+    var obj = {name: 'Siradanai', age: 20, city: 'Bangkok'};
+    if (req.url === '/') {
+        res.write('You are at home page');
+        res.write('\n');
+        res.write(JSON.stringify(obj));
+        res.end();
+    }
+});
 server.addListener('connection', function(socket) {
-    console.log('Client connected')
-})
-server.listen(3000)
-console.log('Listening form port 3000')
+    console.log('Client connected');
+});
+server.listen(3000);
+console.log('Listening form port 3000');
