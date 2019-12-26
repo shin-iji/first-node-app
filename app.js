52ab920622d9e8915bf78471170069d9a588bc2a
Shindanai <siradanai27@gmail.com>
2019-12-26 14:31:39 +0700

    Asynchronous Operation Example

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
-const {getSecretKey, databaseName} = require('./setting')
-console.log(getSecretKey())
-console.log(databaseName)
+const step1 = () => {
+    setTimeout(() => {
+        console.log('The First Step.')
+    }, 3000)
+}
+const step2 = () => {
+    console.log('The Second Step')
+}
+step1()
+step2()
