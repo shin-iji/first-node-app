378263a71eb51ec0a591b9c8e5332e6b8d7c4e3c
Shindanai <siradanai27@gmail.com>
2019-12-26 16:13:58 +0700

    Call Stack and Event Loop

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const myAsync = (callback) => {
-    console.log('Start');
+    console.log('Begin');
     setTimeout(() => {
-        const result = callback('Hello');
+        const result = callback('First');
+        console.log(result);
+    }, 2000);
+    setTimeout(() => {
+        const result = callback('Second');
+        console.log(result);
+    }, 1000);
+    setTimeout(() => {
+        const result = callback('Third');
         console.log(result);
     }, 0);
-    console.log('Finish');
+    console.log('End');
 }
 myAsync((message) => {
-    return message + ' Async.';
+    return message;
 });
